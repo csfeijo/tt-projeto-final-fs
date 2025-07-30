4c563d956ca3bb56a513d302c5f8ccb0b9e627b0
Cícero Feijó <cicero.feijo@gmail.com>
2025-07-30 19:45:01 -0300

    feat: configura o tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 function App() {
   return (
-    <>
+    <div className="p-10 bg-cyan-950 text-white">
       <h1>Projeto Final em React</h1>
-      <h2>Consumindo uma API REST</h2>
+      <h2 className="text-yellow-300">Consumindo uma API REST</h2>
       <hr/>
-    </>
+      <button className="
+        px-4 
+        py-2 
+        bg-cyan-300 
+        cursor-pointer
+        rounded-xl 
+        mt-4 
+        text-cyan-950 
+        border-2 
+        hover:border-cyan-300
+        hover:bg-cyan-950
+        hover:text-white
+      ">
+        ENVIAR
+      </button>
+    </div>
   )
 }
 
